4b828c544f1b78dc43ceb0846e180ac8ad03520c
deysiquispe12 <deysiquispe1296@gmail.com>
2026-08-26 16:33:51 -0500

    cambio en activty

--- app/src/main/java/pe/edu/unu/calculadora/MainActivity.kt
+++ app/src/main/java/pe/edu/unu/calculadora/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        //recuperando los componentes del xml
+        //recuperando los componentes del xml2
         val etNum1 = findViewById<EditText>(R.id.etNumber1)
         val etNum2 = findViewById<EditText>(R.id.etNumber2)
         val rgOptions = findViewById<RadioGroup>(R.id.rgOperations)
